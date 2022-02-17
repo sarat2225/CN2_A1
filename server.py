42ed15addefa9809941b857984d37af23f71dd90
gokul-gk-02 <55867929+gokul-gk-02@users.noreply.github.com>
2022-02-17 23:08:34 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
+from asyncio.windows_events import NULL
 import socket
 import os
+import math
+import time
 
+# 10.0.0.253 - server
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 9999
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 8192
 FORMAT = 'utf-8'
+
+packets_dict = {}
+ack = []
 
 server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server.bind((HOST, PORT))
@@ -19,26 +26,60 @@ while True:
     file_name=data.decode(FORMAT)
 
     server.sendto(file_name.encode(FORMAT),addr)
-    file_name = "udpuser\\" + file_name
 
     if not os.path.exists(file_name):
         print("Requested file doesn't exist")
-        str = "NO"
-        server.sendto(str.encode(FORMAT), addr)
+        string = "NO"
+        server.sendto(string.encode(FORMAT), addr)
         continue
     
-    str = "YES"
-    server.sendto(str.encode(FORMAT), addr)
+    string = "YES"
+    server.sendto(string.encode(FORMAT), addr)
+
+    file_size = os.path.getsize(file_name)
+    total_packets = math.ceil(file_size / 8187)
+
+    for i in range(total_packets):
+        ack.append(0)
+
+    string = str(total_packets)
+    server.sendto(string.encode(FORMAT), addr)
 
     f=open(file_name,"rb")
-    data = f.read(BUFFER_SIZE)
+    data = f.read(BUFFER_SIZE - 5)
+    p_id = 0
     packets = 0
-    print ("Sending ...")
     while (data):
-        if(server.sendto(data,addr)):
-            packets += 1
-            data = f.read(BUFFER_SIZE)
+        str_pid = str(p_id)
+        str_pid = '0' * (5 - len(str_pid)) + str_pid
+        data = str_pid.encode(FORMAT) + data
+        packets_dict[p_id] = data
+        data = f.read(BUFFER_SIZE-5)
+        p_id += 1
+
+    print(len(packets_dict.keys()))
+    print ("Sending ...")
+ 
+    for pid in range(total_packets):
+        server.sendto(packets_dict[pid], addr)
+        packets += 1
+        try:
+            server.settimeout(0.01)
+            data, addr = server.recvfrom(BUFFER_SIZE)
+            ind = int(data.decode(FORMAT))
+            ack[ind] = 1
+        except socket.timeout:
+            print("ACK not received")
+            print(pid)
+            continue
+
     print("Sent requested file")
     print(f"No. of packets sent: {packets}")
-    print(f"No. of bytes sent: {os.path.getsize(file_name)}")
+    print(f"No. of bytes sent: {file_size}")
+
+    for i in range(len(ack)):
+        if ack[i] == 0:
+            print(i)
+
+    time.sleep(20)
     f.close()
