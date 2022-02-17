c99e08d48ed7c668d939ef05a20a7b6352d50642
gokul-gk-02 <55867929+gokul-gk-02@users.noreply.github.com>
2022-02-17 23:08:16 +0530

    Update client.py

--- client.py
+++ client.py
@@ -8,9 +8,11 @@ HOST = "192.168.0.116"
 PORT = 9999
 ADDR = (HOST, PORT)
 
+file_dict = {}
+
 client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 8192
 FORMAT = 'utf-8' 
 
 print(f"Client starting in {HOST}...")
@@ -36,16 +38,26 @@ while True:
         continue
 
     data,addr = client.recvfrom(BUFFER_SIZE)
+    total_packets = int(data.decode(FORMAT))
+    print(total_packets)
+
+    data,addr = client.recvfrom(BUFFER_SIZE)
 
     print ("Receiving File:",fn.decode(FORMAT))
     f = open(fn,'wb')
+    
     start = timeit.default_timer()
     try:
         while(data):
-            f.write(data)
+            p_id = int(data[:5])
+            data = data[5:]
+
+            file_dict[p_id] = data
+
             client.settimeout(2)
             packets += 1
             data,addr = client.recvfrom(BUFFER_SIZE)
+            client.sendto(str(p_id).encode(FORMAT), addr)
     except socket.timeout:
         f.close()
         stop = timeit.default_timer()
@@ -56,4 +68,4 @@ while True:
         print(f"No. of bytes received: {size}")
         print(f"Time Taken: {time_taken} s")
         print(f"Speed: {(size/time_taken)/1024: .2f} kB/s")
-        print()
+        print(len(file_dict.keys()))
