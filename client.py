665fb6ea7859c1b4cf64fedd7d5a59b02718fd2b
gokul-gk-02 <55867929+gokul-gk-02@users.noreply.github.com>
2022-02-18 11:43:21 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,71 +1,76 @@
 import socket
 import os
-from time import time
-import timeit
+import math
+import time
 
-# HOST = socket.gethostbyname(socket.gethostname())
-HOST = "192.168.0.116"
+# 10.0.0.253 - server
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 9999
+BUFFER_SIZE = 8192
+FORMAT = 'utf-8'
 ADDR = (HOST, PORT)
 
-file_dict = {}
+packets_dict = {}
+ack = []
 
 client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-BUFFER_SIZE = 8192
-FORMAT = 'utf-8' 
-
-print(f"Client starting in {HOST}...")
-print("\n")
+print(f"Client connecting to {HOST}...")
 
 while True:
 
-    file_name = input("Enter file Name: ")
+    file_name = input("Enter file u want to send: ")
 
-    client.sendto(file_name.encode(FORMAT), ADDR)
+    client.sendto(file_name.encode(FORMAT),ADDR)
 
-    data,addr = client.recvfrom(BUFFER_SIZE)
-    fn = data.strip()
-
-
-    packets = 0
-    data,addr = client.recvfrom(BUFFER_SIZE)
-
-
-    if data.decode(FORMAT) == "NO":
+    if not os.path.exists(file_name):
         print("File doesn't exist")
-        print("\n")
         continue
 
-    data,addr = client.recvfrom(BUFFER_SIZE)
-    total_packets = int(data.decode(FORMAT))
-    print(total_packets)
+    file_size = os.path.getsize(file_name)
+    total_packets = math.ceil(file_size / 8187)
 
-    data,addr = client.recvfrom(BUFFER_SIZE)
+    for i in range(total_packets):
+        ack.append(0)
 
-    print ("Receiving File:",fn.decode(FORMAT))
-    f = open(fn,'wb')
-    
-    start = timeit.default_timer()
-    try:
-        while(data):
-            p_id = int(data[:5])
-            data = data[5:]
+    string = str(total_packets)
+    client.sendto(string.encode(FORMAT), ADDR)
 
-            file_dict[p_id] = data
+    f=open(file_name,"rb")
+    data = f.read(BUFFER_SIZE - 5)
+    p_id = 0
+    packets = 0
+    while (data):
+        str_pid = str(p_id)
+        str_pid = '0' * (5 - len(str_pid)) + str_pid
+        data = str_pid.encode(FORMAT) + data
+        packets_dict[p_id] = data
+        data = f.read(BUFFER_SIZE-5)
+        p_id += 1
 
-            client.settimeout(2)
-            packets += 1
-            data,addr = client.recvfrom(BUFFER_SIZE)
-            client.sendto(str(p_id).encode(FORMAT), addr)
-    except socket.timeout:
-        f.close()
-        stop = timeit.default_timer()
-        time_taken = stop - start - 2
-        size = os.path.getsize(file_name)
-        print ("File Downloaded")
-        print(f"No. of packets received: {packets}")
-        print(f"No. of bytes received: {size}")
-        print(f"Time Taken: {time_taken} s")
-        print(f"Speed: {(size/time_taken)/1024: .2f} kB/s")
-        print(len(file_dict.keys()))
+    print(len(packets_dict.keys()))
+    print ("Sending ...")
+ 
+    for pid in range(total_packets):
+        client.sendto(packets_dict[pid], ADDR)
+        packets += 1
+        try:
+            client.settimeout(0.01)
+            data, addr = client.recvfrom(BUFFER_SIZE)
+            ind = int(data.decode(FORMAT))
+            ack[ind] = 1
+        except socket.timeout:
+            print("ACK not received")
+            print(pid)
+            continue
+
+    print("Sent requested file")
+    print(f"No. of packets sent: {packets}")
+    print(f"No. of bytes sent: {file_size}")
+
+    for i in range(len(ack)):
+        if ack[i] == 0:
+            print(i)
+
+    time.sleep(20)
+    f.close()
